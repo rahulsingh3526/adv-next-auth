f86c1227e23200e09a1fa45eb270f5060b8ea39c
Rahul_Singh <koderrahul@gmail.com>
2024-05-24 17:25:58 +0530

    feat: Login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,39 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <Button size="lg" variant="ghost">
-      Click Me
-    </Button>
+    <main
+      className="flex h-full flex-col items-center justify-center
+    
+    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 
+    to-blue-800"
+    >
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg"> A simple authentication service</p>
+        <LoginButton>
+          {" "}
+          <Button variant="secondary" size="lg">
+            Sign In
+          </Button>
+        </LoginButton>
+      </div>
+    </main>
   );
 }
---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { Header } from "./header";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+    </Card>
+  );
+};
